1b326e27872d2e02ef666ab00a73f234d084ec8d
Jozef Pecha <70802421+jozefpecha@users.noreply.github.com>
2021-03-16 12:50:37 +0100

    Added comments

--- visualize_pharms.py
+++ visualize_pharms.py
@@ -2,6 +2,15 @@ from pymol import cmd
 
 @cmd.extend
 def pharm_view(pharm_name, mols_name, s_scale = 0.35, s_transparency = 0.5):
+	"""
+	Makes specified changes to pharmacophore and molecule representations
+	
+	:param pharm_name: name of pharmacophore object
+	:param mols_name: name of molecule object
+	:param s_scale: size of spheres representing pharmacophore features (default = 0.35)
+	:param s_transparency: transparency of spheres representing pharmacophore features (default = 0.5)
+	"""
+	
 	cmd.show("spheres", pharm_name)
 	cmd.show("sticks", mols_name)
 	cmd.set('sphere_scale', value=s_scale, selection=pharm_name, state=0)
